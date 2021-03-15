9e6cca9a2661ea6516af2872fef6ce70313e5611
Karol Rutkowski <72763056+szarl@users.noreply.github.com>
2021-03-15 13:17:23 +0100

    Update Main.java

--- src/com/tutorial/Main.java
+++ src/com/tutorial/Main.java
@@ -3,6 +3,8 @@ package com.tutorial;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.print("Hello World");
+        @Override
+        File plik = new File("stworz_plik.txt");
     }
 }
